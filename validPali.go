2c5013f0c565dea852c1c61c3b80474468e53a66
JoshDPT <jjewcu@gmail.com>
2023-05-12 16:16:21 -0400

    add valid pali

---
+++ validPali.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+func validPali (s string) bool {
+	lower := convStrLowerAlphaNum(s)
+	l := 0
+	r := len(lower)-1
+
+	for l < r {
+		if lower[l] != lower[r] {
+			return false
+		}
+		l+=1
+		r-=1
+	}
+	return true
+}
+
+func convStrLowerAlphaNum (s string) string {
+	str := ""
+	lower := strings.ToLower(s)
+	for _, r := range lower {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			str += string(r)
+		}
+	}
+	return str
+}
---
+++ validPali_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestCalc6(t *testing.T) {
+	result1 := validPali("1234")
+	if result1 != false {
+		t.Errorf("Expected result to be false, but got %t", result1)
+	}
+	result2 := validPali("abcdcba")
+	if result2 != true {
+		t.Errorf("Expected result to be true, but got %t", result2)
+	}
+	result3 := validPali("Abc, !dc,   ba   #%")
+	if result3 != true {
+		t.Errorf("Expected result to be true, but got %t", result3)
+	}
+}
