71e88d479961864b79e0dc520b80d1d1725b6d4d
JoshDPT <jjewcu@gmail.com>
2023-06-02 20:34:40 -0400

    update submarine dive

---
+++ 2101_detonate_maximum_bombs_test.go
@@ -0,0 +1,10 @@
+package main
+
+// Input: bombs = [[2,1,3],[6,1,4]]
+// Output: 2
+
+// Input: bombs = [[1,1,5],[10,10,5]]
+// Output: 1
+
+// Input: bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]]
+// Output: 5
--- submarine_dive.go
+++ submarine_dive.go
@@ -3,24 +3,56 @@ package main
 import (
 	"strings"
 	"strconv"
+	"log"
 )
 
-func submarineDive (s string) int {
-	x, y := 0, 0
-	arr := strings.Fields(s)
-	for i,c := range arr {
-		if i == len(arr)-1 {
-			continue
-		} 
-		n,_ := strconv.Atoi(arr[i+1])
-		switch c {
-		case "forward":
-			x += n
-		case "down":
-			y += n
-		case "up":
-			y -= n
+type Tuple struct {
+	x int
+	y int
+}
+
+func parseLine (line string) Tuple {
+	arr := strings.Fields(line)
+	dir := arr[0]
+	amount, err := strconv.Atoi(arr[1])
+
+	if err != nil {
+		log.Fatal("This should not happen")
+	}
+
+	switch dir {
+		case "forward": {
+			return Tuple{
+				x: amount,
+				y: 0,
+			}
+		}
+		case "down": {
+			return Tuple{
+				x: 0,
+				y: amount,
+			}
+		}
+		case "up": {
+			return Tuple{
+				x: 0,
+				y: -amount,
+			}
 		}
 	}
-	return x * y;
+	return Tuple{0,0}
+}
+
+func submarineDive (s string) int {
+		lines := strings.Split(s, "\n")
+		
+		pos := Tuple{0,0}
+
+		for _, line := range lines {
+			coord := parseLine(line)
+			pos.x += coord.x
+			pos.y += coord.y
+		}
+
+		return pos.x * pos.y
 }
